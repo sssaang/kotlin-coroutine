8ac89b29dafad17a2dfa398346e0d365e937d3ef
Sang <thanato0912@gmail.com>
2022-06-26 20:20:24 +0900

    feat: suspend

--- src/tasks/Request4Suspend.kt
+++ src/tasks/Request4Suspend.kt
@@ -1,7 +1,25 @@
 package tasks
 
+import kotlinx.coroutines.launch
 import contributors.*
+import kotlinx.coroutines.*
 
 suspend fun loadContributorsSuspend(service: GitHubService, req: RequestData): List<User> {
-    TODO()
+    val responseRepos = service.getOrgRepos(req.org)
+    logRepos(req, responseRepos)
+
+    val repos = responseRepos.bodyList()
+    val allUsers = mutableListOf<User>()
+
+    val jobs = repos.map { repo ->
+        GlobalScope.launch {
+            val responseUsers = service.getRepoContributors(req.org, repo.name)
+            logUsers(repo, responseUsers)
+            val users = responseUsers.bodyList()
+            allUsers += users
+        }
+    }
+    jobs.joinAll()
+
+    return allUsers.aggregate()
 }
