60ec7746615d3f967a5b8b3e36658618cf648449
Sang <thanato0912@gmail.com>
2022-06-26 20:31:47 +0900

    feat: concurrency

--- src/tasks/Request5Concurrent.kt
+++ src/tasks/Request5Concurrent.kt
@@ -4,5 +4,18 @@ import contributors.*
 import kotlinx.coroutines.*
 
 suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> = coroutineScope {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+
+    val tasks = repos.map { repo ->
+        async {
+            val responseUsers = service.getRepoContributors(req.org, repo.name)
+            logUsers(repo, responseUsers)
+            return@async responseUsers.bodyList()
+        }
+    }
+
+    return@coroutineScope tasks.awaitAll().flatten().aggregate()
 }
